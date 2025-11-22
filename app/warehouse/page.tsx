1cc6897bd3741ed4b8e48954dc782b4f4099f652
Bala Sudalaimuthu <bala@Balas-MacBook-Air.local>
2025-11-22 10:02:42 +0530

    Added all routes

---
+++ app/warehouse/page.tsx
@@ -0,0 +1,11 @@
+export default function Warehouse() {
+    return (
+        <div className="flex min-h-screen items-center justify-center bg-background font-sans"> 
+            <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-foreforeground sm:items-start">
+                <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-foreground">
+                    Welcome to the Warehouse Page!
+                </h1>
+            </main>
+        </div>
+    );
+}
